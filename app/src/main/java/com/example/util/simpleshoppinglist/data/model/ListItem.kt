3a5a08d84007f63b9a454dc9ebcb8bbb65d7d527
Razeeman <freeraz@mail.ru>
2019-03-10 11:37:33 +0300

    Added room.

--- app/src/main/java/com/example/util/simpleshoppinglist/data/model/ListItem.kt
+++ app/src/main/java/com/example/util/simpleshoppinglist/data/model/ListItem.kt
@@ -1,10 +1,19 @@
 package com.example.util.simpleshoppinglist.data.model
 
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.util.*
 
 /**
  * Model class for shopping list item.
  */
-class ListItem (var id: String = UUID.randomUUID().toString(),
+@Entity(tableName = "list_items")
+class ListItem (@PrimaryKey @ColumnInfo(name = "id")
+                var id: String = UUID.randomUUID().toString(),
+
+                @ColumnInfo(name = "name")
                 var name: String = "",
+
+                @ColumnInfo(name = "color")
                 var color: Int = 0)
