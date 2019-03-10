a533cfbb0f33501dade32fb84dd063b95fbcca50
Razeeman <freeraz@mail.ru>
2019-03-10 11:58:53 +0300

    Added DAO for list items.

---
+++ app/src/main/java/com/example/util/simpleshoppinglist/data/db/ListItemDao.kt
@@ -0,0 +1,59 @@
+package com.example.util.simpleshoppinglist.data.db
+
+import androidx.room.*
+import com.example.util.simpleshoppinglist.data.model.ListItem
+
+/**
+ * Data access object for list items database table.
+ */
+@Dao
+interface ListItemDao {
+
+    /**
+     * Select all list items.
+     *
+     * @return list of all items.
+     */
+    @Query("SELECT *")
+    fun getAll(): List<ListItem>
+
+    /**
+     * Select list item with certain id.
+     *
+     * @param id id of the item to select.
+     * @return   item with specified id, null if not found.
+     */
+    @Query("SELECT * FROM list_items WHERE id = :id LIMIT 1")
+    fun getById(id: String): ListItem?
+
+    /**
+     * Insert list item into database.
+     *
+     * @param listItem item to insert.
+     */
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(listItem: ListItem)
+
+    /**
+     * Update list item in the database.
+     *
+     * @param listItem item to update.
+     */
+    @Update
+    fun update(listItem: ListItem)
+
+    /**
+     * Delete list item from the database.
+     *
+     * @param listItem item to delete.
+     */
+    @Delete
+    fun delete(listItem: ListItem)
+
+    /**
+     * Delete all list items from the database.
+     */
+    @Query("DELETE FROM list_items")
+    fun deleteAll()
+
+}
