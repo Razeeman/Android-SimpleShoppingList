db1f3313cc76c84dd03b78914542b68e04848195
Razeeman <freeraz@mail.ru>
2019-03-11 20:57:29 +0300

    Added empty test for items repository to fill later.

---
+++ app/src/androidTest/java/com/example/util/simpleshoppinglist/data/repo/ItemsRepositoryTest.kt
@@ -0,0 +1,40 @@
+package com.example.util.simpleshoppinglist.data.repo
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class ItemsRepositoryTest {
+
+    @Before
+    fun setUp() {
+        // TODO add tests.
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun loadListItems() {
+    }
+
+    @Test
+    fun loadListItem() {
+    }
+
+    @Test
+    fun saveListItem() {
+    }
+
+    @Test
+    fun deleteListItem() {
+    }
+
+    @Test
+    fun deleteAllListItems() {
+    }
+}
