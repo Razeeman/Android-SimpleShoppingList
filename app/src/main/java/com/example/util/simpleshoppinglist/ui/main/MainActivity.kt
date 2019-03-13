629521bc390f77e8ea2a628dc3851d27f9ac2f6c
Razeeman <freeraz@mail.ru>
2019-03-13 20:39:51 +0300

    Refactored.

--- app/src/main/java/com/example/util/simpleshoppinglist/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/util/simpleshoppinglist/ui/main/MainActivity.kt
@@ -1,19 +1,18 @@
-package com.example.util.simpleshoppinglist
+package com.example.util.simpleshoppinglist.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.util.simpleshoppinglist.R
 import com.example.util.simpleshoppinglist.data.db.AppDatabase
 import com.example.util.simpleshoppinglist.data.model.ListItem
 import com.example.util.simpleshoppinglist.data.repo.ItemsRepository
-import com.example.util.simpleshoppinglist.ui.main.MainFragment
-import com.example.util.simpleshoppinglist.ui.main.MainPresenter
 import com.example.util.simpleshoppinglist.util.AppExecutors
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.main_activity)
 
         val mainFragment = supportFragmentManager.findFragmentById(R.id.content_frame) as MainFragment?
             ?: MainFragment.newInstance().also {
