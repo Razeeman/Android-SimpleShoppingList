3e470501040059ac4d38ad73b29351d9eea9303a
Razeeman <freeraz@mail.ru>
2019-03-14 19:53:34 +0300

    Changed item view to rounded drawable.

--- app/src/main/java/com/example/util/simpleshoppinglist/ui/custom/ItemHolder.kt
+++ app/src/main/java/com/example/util/simpleshoppinglist/ui/custom/ItemHolder.kt
@@ -1,21 +1,28 @@
 package com.example.util.simpleshoppinglist.ui.custom
 
+import android.graphics.PorterDuff
+import android.graphics.PorterDuffColorFilter
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.util.simpleshoppinglist.R
 import com.example.util.simpleshoppinglist.data.model.ListItem
 import kotlinx.android.synthetic.main.item_layout.view.*
 
+
 /**
  * Holder for RecyclerView of ListItems.
  */
-class ItemHolder(inflater: LayoutInflater, parent: ViewGroup)
+class ItemHolder(inflater: LayoutInflater, private val parent: ViewGroup)
     : RecyclerView.ViewHolder(inflater.inflate(R.layout.item_layout, parent, false)) {
 
     fun bind(listItem: ListItem) {
+        val drawable = ContextCompat.getDrawable(parent.context.applicationContext, R.drawable.item_drawable)
+        drawable?.colorFilter = PorterDuffColorFilter(listItem.color, PorterDuff.Mode.SRC_IN)
+
         itemView.tv_item.text = listItem.name
-        itemView.tv_item.setBackgroundColor(listItem.color)
+        itemView.tv_item.background = drawable
     }
 
 }
--- app/src/main/java/com/example/util/simpleshoppinglist/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/util/simpleshoppinglist/ui/main/MainActivity.kt
@@ -34,10 +34,10 @@ class MainActivity : AppCompatActivity() {
         itemsRepository.apply {
             deleteAllItems()
             saveItem(ListItem(name = "item 1", color = 0xFF2196f3.toInt()))
-            saveItem(ListItem(name = "item 2", color = 0xFF8e24aa.toInt()))
+            saveItem(ListItem(name = "item 2 with long name", color = 0xFF8e24aa.toInt()))
             saveItem(ListItem(name = "item 3", color = 0xFFf50057.toInt()))
             saveItem(ListItem(name = "item 4", color = 0xFF00c853.toInt()))
-            saveItem(ListItem(name = "item 5", color = 0xFFffb300.toInt()))
+            saveItem(ListItem(name = "item 5 with very very long name", color = 0xFFffb300.toInt()))
         }
     }
 }
