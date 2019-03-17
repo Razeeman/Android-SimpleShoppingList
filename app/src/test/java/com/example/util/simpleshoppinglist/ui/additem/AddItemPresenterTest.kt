96afa4ec4d340c85e9d281ca6e9b123755458caa
Razeeman <freeraz@mail.ru>
2019-03-17 12:03:27 +0300

    Added empty presenter tests to fill out later.

---
+++ app/src/test/java/com/example/util/simpleshoppinglist/ui/additem/AddItemPresenterTest.kt
@@ -0,0 +1,40 @@
+package com.example.util.simpleshoppinglist.ui.additem
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class AddItemPresenterTest {
+
+    // TODO add tests
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun loadData() {
+    }
+
+    @Test
+    fun addItemToList() {
+    }
+
+    @Test
+    fun saveItem() {
+    }
+
+    @Test
+    fun attachView() {
+    }
+
+    @Test
+    fun detachView() {
+    }
+}
---
+++ app/src/test/java/com/example/util/simpleshoppinglist/ui/main/MainPresenterTest.kt
@@ -0,0 +1,40 @@
+package com.example.util.simpleshoppinglist.ui.main
+
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainPresenterTest {
+
+    // TODO add tests
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+
+    @Test
+    fun loadData() {
+    }
+
+    @Test
+    fun removeItemFromList() {
+    }
+
+    @Test
+    fun clearList() {
+    }
+
+    @Test
+    fun attachView() {
+    }
+
+    @Test
+    fun detachView() {
+    }
+}
